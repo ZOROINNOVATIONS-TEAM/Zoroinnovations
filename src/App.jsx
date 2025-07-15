e2815473035cfa143a35875084b66d0339035fa2
shashwat shukla <shuklashashwat009@gmail.com>
2025-07-15 13:52:05 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className=''>
+       home where routes will be
+    </div>
+  )
+}
+
+export default App
