61e0747101655a29f04a7806c9e62fb449f68720
shashwat shukla <shuklashashwat009@gmail.com>
2025-07-23 18:59:36 +0530

    approved changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
 import { Routes, Route} from 'react-router-dom'
-import Header from './pages/Header'
 import  EmployeeVer from './pages/EmployeeVer.jsx'
 
 function App() {
   return (
     <>
-    <Header/>
     <Routes>
        <Route path="/" element={<EmployeeVer/>} />
     </Routes>
