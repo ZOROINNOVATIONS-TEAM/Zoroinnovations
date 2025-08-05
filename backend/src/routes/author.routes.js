ecf4745e80c57806746932c4659a3f4f8ff0a57a
Tejas <tejasambore2003@gmail.com>
2025-08-05 14:04:58 +0530

    Delete folder

--- backend/src/routes/author.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAuthors, getAuthorWithArticles } from '../controllers/author.controller.js';
-
-const router = express.Router();
-
-router.get('/', getAuthors);
-router.get('/:id', getAuthorWithArticles);
-
-export default router;
--- backend/src/routes/public.routes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { submitMessage } from '../controllers/public.controller.js';
-
-const router = express.Router();
-
-// Message Submission
-router.post('/contacts', submitMessage);
-
-export default router;
