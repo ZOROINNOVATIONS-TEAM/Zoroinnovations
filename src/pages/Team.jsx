e80202d44dfecb1add89a3002cb9552b9d48f86b
Harshitha-HG <harshithahrgopal@gmail.com>
2025-08-05 08:25:06 +0530

    Update Team.jsx

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Header from '../components/TeamPage/Header';
 import TeamMembers from '../components/TeamPage/TeamMembers';
 import EmployeeTable from '../components/TeamPage/EmployeeTable';
 import AddTeamMemberForm from '../components/TeamPage/AddTeamMemberForm';
@@ -8,7 +7,6 @@ import Footer from '../components/TeamPage/Footer';
 const Team = () => {
   return (
     <div className="w-full min-h-screen relative font-sans">
-      <Header />
       <div className="pt-20 w-full bg-gradient-to-br from-blue-900 via-blue-500 to-orange-500 p-8">
         <div className="max-w-[1440px] mx-auto">
           <h1 className="text-3xl font-bold text-white mb-2">Team Management</h1>
