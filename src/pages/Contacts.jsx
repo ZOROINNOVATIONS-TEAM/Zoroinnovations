ef0b07ebf29e62535a1ea98b2afa30467a9be801
Harshitha-HG <harshithahrgopal@gmail.com>
2025-08-06 21:37:21 +0530

    Update Contacts.jsx

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -3,7 +3,7 @@ import Footer from '../components/loginPage/LoginFooter';
 
 const Contacts = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gradient-to-br from-blue-900 via-indigo-600 to-orange-500">
+ <div className="min-h-screen mt-10 w-full bg-gradient-to-br from-blue-900 via-blue-500 to-orange-500 py-12 px-6">
       <div className="flex-1 p-6">
         <div className="bg-white rounded-xl shadow-xl p-6 max-w-6xl mx-auto">
           <h1 className="text-3xl font-bold text-gray-800 mb-6">Contacts or Inquiries</h1>
