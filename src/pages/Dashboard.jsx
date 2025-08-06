44ed6ed386865b557ba878bbe5f141d16453ce26
Harshitha-HG <harshithahrgopal@gmail.com>
2025-08-06 21:34:14 +0530

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,6 +7,10 @@ const Dashboard = () => {
   return (
         <div className="min-h-screen mt-10 w-full bg-gradient-to-br from-blue-900 via-blue-500 to-orange-500 py-12 px-6">
       <main className="flex-grow p-6">
+        <h1 className="text-3xl font-bold text-white mb-2">Dashboard</h1>
+          <p className="text-md text-gray-600 text-white mb-6">
+            Welcome to the Zoro Innovations Dashboard. Here's an overview of your system performance.
+          </p>
        
           {/* Top Stats */}
           <div className="flex flex-row gap-8 my-12">
@@ -17,7 +21,7 @@ const Dashboard = () => {
 
           {/* Quick Actions */}
           <div className="mb-12">
-            <h2 className="text-xl font-semibold mb-2">Quick Actions</h2>
+            <h2 className="text-xl font-semibold text-white mb-2">Quick Actions</h2>
             <div className="flex gap-4 mt-4">
               <button className="bg-orange-500 text-white px-4 py-2 rounded-3xl font-bold shadow-md hover:bg-orange-600">
                 Create New Project
@@ -29,7 +33,7 @@ const Dashboard = () => {
           </div>
 
           {/* System Overview */}
-          <h2 className="text-xl font-semibold mb-6">System Overview</h2>
+          <h2 className="text-xl font-semibold text-white mb-6">System Overview</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
             {/* Project Completion Chart */}
             <div className="bg-white text-gray-900 p-6 rounded-lg shadow-md">
