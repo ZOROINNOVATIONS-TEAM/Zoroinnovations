f2717c1998ffe3f3990b57e4d147211002204d61
RajathRavikumar <rajathravikumar2205@gmail.com>
2025-08-07 20:09:07 +0530

    Add files via upload

---
+++ src/pages/Project.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./ContactComponents/Header";
+import Footer from "./ContactComponents/Footer";
+import UserMessages from "./ContactComponents/UserMessages";
+import ProjectsDashboard from "./ContactComponents/ProjectDashboard";"./components//ProjectDashboard";
+import "./index.css"; // Assuming you have a CSS file for styling
+
+const ContactPage = () => {
+  return (
+    <div className="app">
+      <Header />
+      <ProjectsDashboard />
+      <Footer />
+    </div>
+  );
+};
+
+export default ContactPage;
