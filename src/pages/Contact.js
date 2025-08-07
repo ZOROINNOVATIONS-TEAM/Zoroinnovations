e21fe548f0a7b178d52f9aa36bcb71edd1e3bbe1
RajathRavikumar <rajathravikumar2205@gmail.com>
2025-08-07 20:06:49 +0530

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,14 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import UserMessages from "./components/UserMessages";
-import ProjectsDashboard from "./components/ProjectDashboard";"./components//ProjectDashboard";
-import "./index.css"; // Assuming you have a CSS file for styling
+import "./index.css";
 
 const ContactPage = () => {
   return (
     <div className="app">
       <Header />
-      <ProjectsDashboard />
+      <UserMessages />
       <Footer />
     </div>
   );
